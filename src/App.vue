6fc0a767252e2decc174513ee715a9b684d18bc5
Mario Seidl <mario.s-@live.de>
2021-10-26 11:00:24 +0200

    project setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+  export default {
+    name: 'app',
+    components:{
+        
+    }
+  };
+</script>
+<template>
+  
+</template>
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {createApp} from "vue";
+import App from './App.vue';
+ 
+let app = createApp(App);
+
+let vm = app.mount('#root');
